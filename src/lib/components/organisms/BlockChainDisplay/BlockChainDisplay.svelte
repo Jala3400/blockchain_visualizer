3fbb7fd05e04cffdba5601db778feee13c1f8b10
Jala3400 <jala.3400@gmail.com>
2025-05-11 21:30:55 +0200

    Start by default with more blocks

--- src/lib/components/organisms/BlockChainDisplay/BlockChainDisplay.svelte
+++ src/lib/components/organisms/BlockChainDisplay/BlockChainDisplay.svelte
@@ -2,13 +2,21 @@
     import BlockCard from "$lib/components/molecules/BlockCard/BlockCard.svelte";
     import { Block } from "$lib/classes/Block";
 
-    // Array to store block instances
-    const block = new Block(
+    // Create initial blocks
+    const genesisBlock = new Block(
         "0000000000000000000000000000000000000000000000000000000000000000",
         "",
         2
     );
-    let blocks: Block[] = [block];
+
+    // Create second block with previous hash from genesis block
+    const secondBlock = new Block(genesisBlock.getHash(), "Second Block", 2);
+
+    // Create third block with previous hash from second block
+    const thirdBlock = new Block(secondBlock.getHash(), "Third Block", 2);
+
+    // Initialize with 3 blocks
+    let blocks: Block[] = [genesisBlock, secondBlock, thirdBlock];
 
     // Handle data changes in any block
     function handleDataChange(index: number) {
@@ -18,12 +26,19 @@
             blocks[i].updatePrevHash(prevHash);
         }
     }
+
+    // Function to add a new block to the blockchain
+    function addBlock() {
+        const prevHash = blocks[blocks.length - 1].getHash();
+        const newBlock = new Block(prevHash, "", 2);
+        blocks = [...blocks, newBlock];
+    }
 </script>
 
 <div class="blockchain">
     {#each blocks as block, i}
         <div class="block-wrapper">
-            <h3>Block #{i}</h3>
+            <h3>Block #{i + 1}</h3>
             <BlockCard
                 {block}
                 prevHash={i > 0 ? blocks[i - 1].getHash() : "0"}
@@ -31,6 +46,10 @@
             />
         </div>
     {/each}
+
+    <div class="add-block-container">
+        <button onclick={addBlock}>Add New Block</button>
+    </div>
 </div>
 
 <style>
@@ -51,5 +70,17 @@
     .block-wrapper h3 {
         text-align: center;
         margin-bottom: 10px;
+    }
+
+    .add-block-container {
+        margin-top: 10px;
+        width: 100%;
+        display: flex;
+        justify-content: center;
+    }
+
+    button {
+        padding: 8px 16px;
+        font-size: 1rem;
     }
 </style>
